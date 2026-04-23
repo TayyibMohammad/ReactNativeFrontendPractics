58696b9d67ba93084b936ed9d54737c5070c1f31
Mohammad Tayyib <tayyibmohammad2005@gmail.com>
2026-04-23 23:00:31 +0530

    Added Error page

--- src/app/screens/[id].tsx
+++ src/app/screens/[id].tsx
@@ -4,6 +4,7 @@ import { useLocalSearchParams } from 'expo-router'
 import  projects  from '@/constants/Projects'
 import SignInScreen from '@/pages/SignInScreen'
 import { Stack} from 'expo-router'
+import Line from '@/components/Line'
 
 
 
@@ -27,8 +28,12 @@ export default function tom() {
          options={{headerTitle: "404 Page ⚔️", headerStyle: { backgroundColor: '#1e293b' }, headerTitleStyle: { color: '#f1f5f9' }, headerTintColor: '#f1f5f9',}} />
 
             
-            <View>
-            <Text>404! Page</Text>
+            <View className='flex-1 bg-[#000000] items-center justify-center'>
+                <View className="border-2 border-red-400 bg-slate-800 p-8 rounded-lg">
+                    <Text className='color-red-600 font-bold text-2xl pb-4'>Error !! 404 Page Not Found</Text>
+                    <Line/>
+                    <Text className='color-slate-400 text-lg'>The page you are looking for does not exist.</Text>
+                </View>
             </View>
         </>
   )
---
+++ src/components/Line.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Line() {
+  return (
+    <View className='h-[1px] bg-zinc-300 my-4'>
+      
+    </View>
+  )
+}
